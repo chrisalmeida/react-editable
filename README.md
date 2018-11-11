e9e0c31ec88ae053450bb76f72af958a4671e818
Chris Almeida <thechrisalmeida@gmail.com>
2018-11-10 19:09:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# React Editable
+
+Making use of react hooks for editing React components in place.
+
+### Installation
+
+```
+npm install --save chrisalmeida/react-editable#master
+```
+
+### Prerequisites
+
+React >= v16.7.0-alpha is needed in order to use hooks.
+
+### Example
+
+```jsx
+<Editable
+  render={(editable, setEditing) => {
+    return (
+      <input
+        className={!editable ? 'not-editing' : ''}
+        onFocus={e => setEditing(e)}
+      />
+    )
+  }}
+/>
+```
+
+A [create-react-app](https://github.com/facebook/create-react-app) example is included in example/
---
+++ index.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+
+const Editable = ({ render, eventType }) => {
+  // Set initial state
+  const [editing, setEditing] = useState(false)
+  const [componentClicked, setComponentClicked] = useState(null)
+
+  // Make component uneditable when event occurs outside of this component
+  const handleEvent = ({ target }) => {
+    if (componentClicked)
+      if (!componentClicked.contains(target)) setEditing(false)
+  }
+
+  // Add or remove event listeners
+  const updateListeners = attr => {
+    if (eventType) {
+      document[attr](eventType, handleEvent, false)
+    } else {
+      document[attr]('click', handleEvent, false)
+    }
+  }
+
+  useEffect(() => {
+    // Set event listeners
+    updateListeners('addEventListener')
+
+    return () => {
+      // Remove event listeners
+      updateListeners('removeEventListener')
+    }
+  })
+
+  // Make component editable and capture target
+  // for comparison in handleEvent
+  const userSetEditing = ({ target }) => {
+    setEditing(true)
+    setComponentClicked(target)
+  }
+
+  return render(editing, userSetEditing)
+}
+
+export { Editable }
